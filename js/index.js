b5265841673de5a66d68f12c78db27e25886b6d3
anhlo4ephuong <164766271+anhlo4ephuong@users.noreply.github.com>
2024-07-09 10:51:37 +0700

    xoá hết

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 const { pathname } = window.location;
-await navigator.serviceWorker.register('../sw.js');
-const response = await fetch(`../game.json`);
+const response = await fetch(`https://jacksmithpro.github.io/game.json`);
 const { controls, scale } = response.status === 200 ? await response.json?.() : {};
 window.RufflePlayer = window.RufflePlayer || {};
 const $playground = document.querySelector('.playground');
@@ -13,7 +12,7 @@ const triggerKeydownEvent = event => window.dispatchEvent(new KeyboardEvent('key
 const triggerKeyupEvent = event => window.dispatchEvent(new KeyboardEvent('keyup', event));
 const ruffle = window.RufflePlayer.newest();
 const player = ruffle.createPlayer();
-const gamePath = `../game.swf`;
+const gamePath = `https://jacksmithpro.github.io/game.swf`;
 const exitFullscreen = () => {
     if (document.exitFullscreen && document.fullscreenElement) {
         document.exitFullscreen();
@@ -99,7 +98,7 @@ if (controls?.length) {
             $joystick.addEventListener('joystickup', handleKeyEvents);
         }
         if ('button' === type) {
-            const Button = await import('./button.js');
+            const Button = await import('https://jacksmithpro.github.io/button.js');
             Button.default(control, $controls);
         }
     });
