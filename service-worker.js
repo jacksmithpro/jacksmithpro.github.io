fef7f24e474b2d723b2b3a865bde3c7a4e751277
anhlo4ephuong <164766271+anhlo4ephuong@users.noreply.github.com>
2024-10-15 15:38:58 +0700

    hihi

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'bacon-ipsem-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/js/loader.js',
+  '/assets/luoiho.swf',
+  '/assets/fonts.swf',
+  '/assets/den.png',
+  '/assets/icon-192x192.png',
+  '/assets/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
