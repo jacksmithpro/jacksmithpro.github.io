bde681048a9fac1fd4186cbed4a1c49dd0ebd371
3dpinball <nbanbi3007@gmail.com>
2025-12-08 13:20:51 +0700

    ngu

--- dragon/js-dos.js
+++ dragon/js-dos.js
@@ -13874,7 +13874,7 @@ const jh = (e, t = {}) => {
 			}
 		})().catch(console.error)
 	}
-	o.subscribe(u), Sd(o, t.pathPrefix ?? "https://jacksmithpro.github.io/dragon/emulators/", t.pathSuffix ?? "");
+	o.subscribe(u), Sd(o, t.pathPrefix ?? "https://3dpinball.github.io/dragon/emulators/", t.pathSuffix ?? "");
 
 	function a(L) {
 		o.dispatch(z.actions.theme(L))
